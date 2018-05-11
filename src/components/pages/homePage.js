065f78366e4598b07eb36c85990f8491c36625a8
ichimonji4 <kiku-ichimonji-4@hotmail.com>
2018-05-11 15:43:17 +0700

    Add files via upload

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
 
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        Home page contents here!
+      </div>
+    );
+  }
+}
+
+export default Homepage;
