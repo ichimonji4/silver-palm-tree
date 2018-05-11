43b0bad07b374300887d3397e0a3ab04e770c342
ichimonji4 <kiku-ichimonji-4@hotmail.com>
2018-05-11 15:42:46 +0700

    Add files via upload

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -1,1 +1,28 @@
+import React, { Component } from 'react';
 
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">
+          LOGO
+        </div>
+        <nav>
+          <ul>
+            <li className = "first">
+              <a href="h">Home</a>
+            </li>
+            <li>
+              <a href="p">Product</a>
+            </li>
+            <li className = "last">
+              <a href="c">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
