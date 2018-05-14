b09d4c2278640d282b481d9f9923d2c8e509aa4e
Four <four@northstar.loc>
2018-05-14 13:52:37 +0700

    ns 20180514 0152

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -4,7 +4,7 @@ class Homepage extends Component {
   render() {
     return (
       <div className="container-fluid">
-        Home page contents here!
+        Home page contents here!555
       </div>
     );
   }
