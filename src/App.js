92d19963a9c936b842edfad73af4e29244584e6e
Four <four@northstar.loc>
2018-05-14 10:44:00 +0700

    iv01

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Homepage />
-        Test010203
+        Test01020304
         <Footer />
       </div>
     );
